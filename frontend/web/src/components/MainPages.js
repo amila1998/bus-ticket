eef31c6b29aa7f139b1e7c57d3ed5e9b14058359
VishwaLakpriya <vishwalakpriya99@gmail.com>
2022-10-28 23:17:46 +0530

    Created bus time table

--- frontend/web/src/components/MainPages.js
+++ frontend/web/src/components/MainPages.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Routes, Route, Navigate } from "react-router-dom";
+import BusTimeTable from './busTimeTable/BusTimeTable';
 import ForgotPassword from './forgotPassword/ForgotPassword';
 import Login from './login/Login';
 import ResetPassword from './resetPassword/ResetPassword';
@@ -13,6 +14,8 @@ const MainPages = () => {
         <Route path='/signin' element={<Login />} />
         <Route path='/forgotPassword' element={<ForgotPassword />} />
         <Route path='/resetPassword' element={<ResetPassword/>} />
+        
+        <Route path='/busTimeTable' element={<BusTimeTable/>} />
         </Routes>
   </div>
   )
