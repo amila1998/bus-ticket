2b0696078eccbd8b5fa569b8e2a4b1857f5bb303
sean-udayantha <seanudayantha@mail.com>
2022-11-02 13:56:28 +0530

    Bus Management backend

---
+++ backend/models/busModel.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const busSchema = new Schema(
+    {
+        chassieNo: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        noOfSeat: {
+            type:Number,
+            required: true,
+        },
+        type:{
+            type: String,
+            required: true,
+            enum: ['normal', 'semi','ac'] 
+        },
+        condition:{
+            type: String,
+            required: true,
+        },
+        busNumber:{
+            type:String,
+            required:true,
+        
+        }
+    },
+{ timestamps: true }
+);
+
+const Bus = model("Bus",busSchema);
+
+module.exports = Bus;
