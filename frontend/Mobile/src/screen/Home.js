a3021b7e2ce4cfb61c1a650b1e63e21dd31960ba
amila1998 <amiladevin@gmail.com>
2022-11-03 04:05:44 +0530

    Mobile : View all time table

--- frontend/Mobile/src/screen/Home.js
+++ frontend/Mobile/src/screen/Home.js
@@ -1,16 +1,24 @@
 
+import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 import React from 'react'
 
 import { StyleSheet, Text, View, Button, SafeAreaView, ScrollView, TextInput, TouchableOpacity, Image } from 'react-native';
+import client from '../api/client';
 const Home = () => {
     const [allRoutes, setAllRoutes] = React.useState([])
+    const [startLocation, setStartLocation] = React.useState('')
+    const [endLocation, setEndLocation] = React.useState('')
     console.log("🚀 ~ file: Home.js ~ line 8 ~ Home ~ allRoutes", allRoutes)
+    const Navigator = useNavigation();
+    const navigateViewAllTimeTables = () => {
+        Navigator.navigate('Bus_Time_Tables')
+    }
     React.useEffect(() => {
 
         const getAllRoutes = async () => {
             try {
-                const res = axios.get('http://192.168.43.112:8090/api/timetable/')
+                const res = client.get('/api/timetable/')
                 console.log("🚀 ~ file: Home.js ~ line 14 ~ getAllRoutes ~ res", res)
                 setAllRoutes(res.data.AllTimetables)
             } catch (error) {
@@ -31,7 +39,7 @@ const Home = () => {
                     <View>
                         <View>
                             <Image resizeMode={'contain'} style={styles.defultBg} source={require('../../assets/logo.png')} />
-                          
+
                         </View>
                         <View style={{ padding: 10 }}>
                             <View>
@@ -46,6 +54,11 @@ const Home = () => {
                             <View style={styles.formInput}>
                                 <TouchableOpacity style={styles.defaultButton}>
                                     <Text style={{ textAlign: 'center', fontSize: 16, color: '#fff' }}>View</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.formInput}>
+                                <TouchableOpacity onPress={navigateViewAllTimeTables} style={styles.defaultButton2}>
+                                    <Text style={{ textAlign: 'center', fontSize: 16, color: '#fff' }}>View All Time Tables</Text>
                                 </TouchableOpacity>
                             </View>
                         </View>
@@ -85,7 +98,7 @@ const styles = StyleSheet.create({
     defultBg: {
         width: '100%',
         height: 150,
-        padding:10
+        padding: 10
     },
     loginBody: {
         padding: 10,
@@ -107,6 +120,11 @@ const styles = StyleSheet.create({
     defaultButton: {
         padding: 15,
         backgroundColor: '#fd307b',
+        borderRadius: 50,
+    },
+    defaultButton2: {
+        padding: 15,
+        backgroundColor: '#f5a727',
         borderRadius: 50,
     },
     title: {
